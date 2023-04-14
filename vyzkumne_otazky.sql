937cee1386b561b30c39c3513dd4d2fb42cada5d
Petr Melichařík <petr.melicharik@seznam.cz>
2023-04-14 10:48:13 +0200

    zpracování otázky č.2

---
+++ vyzkumne_otazky.sql
@@ -0,0 +1,42 @@
+-- otázka číslo 1
+
+CREATE OR REPLACE TABLE t_avg_pay_comp 
+AS
+SELECT
+tpmspf.industry_name,
+tpmspf.`year` AS year1,
+ROUND (tpmspf.avarage_pay,0) AS avg_pay_2006,
+tpmspf2.`year` AS year2,
+ROUND (tpmspf2.avarage_pay,0) AS avg_pay_2018
+FROM t_petr_melicharik_sql_primary_final tpmspf
+JOIN t_petr_melicharik_sql_primary_final tpmspf2 
+ON tpmspf.industry_name = tpmspf2.industry_name 
+WHERE tpmspf.`year` = 2006 AND tpmspf2.`year` = 2018
+GROUP BY industry_name 
+;
+
+
+SELECT *,
+CASE 
+	WHEN avg_pay_2018 > avg_pay_2006 THEN 1
+	ELSE 0	
+END AS pay_raise
+FROM t_avg_pay_comp tapc
+;
+
+
+-- otázka číslo 2
+
+SELECT
+`year`,
+round(avg(avarage_pay),0) AS avg_pay_in_year,
+product_name,
+round(avg(avarage_price),1) AS avg_product_price_in_year,
+floor(round(avg(avarage_pay),0)/round(avg(avarage_price),1)) AS quantity_to_buy
+FROM t_petr_melicharik_sql_primary_final tpmspf
+WHERE product_code IN (111301,114201) AND `year` IN (2006, 2018)
+GROUP BY `year`, product_code
+;
+
+-- otázka číslo 3
+
